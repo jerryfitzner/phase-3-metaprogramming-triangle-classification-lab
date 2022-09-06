a6416fe997f3c8d928e3565eee6945854ad72927
Jerry Fitzner <jfitzner1@gmail.com>
2022-09-06 17:10:00 -0400

    Complete Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    triangle_check
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def triangle_check
+    if side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
